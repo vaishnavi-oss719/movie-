4b0a2eab810018f1f8bfde41c8b38457777ee63b
vaishnavi-oss719 <rv172542@gmail.com>
2026-02-07 14:11:50 +0530

    upadate status

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,4 @@
-// import { Navigate } from "react-router-dom";
 
-// export default function ProtectedRoute({ children }) {
-//   const isAuth = localStorage.getItem("isAuth");
-
-//   return isAuth ? children : <Navigate to="/login" />;
-// }
 
 // import { Navigate } from "react-router-dom";
 
