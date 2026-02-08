c15a053909ca1e539edc14a4f3641b8588331c58
vaishnavi-oss719 <rv172542@gmail.com>
2026-02-08 17:34:35 +0530

    Fix ProtectedRoute white screen issue

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -6,6 +6,19 @@
 //   return isAuth ? children : <Navigate to="/login" />;
 // }
 
+// import { Navigate } from "react-router-dom";
+
+// export default function ProtectedRoute({ children }) {
+//   const user = localStorage.getItem("user");
+
+//   if (!user) {
+//     return <Navigate to="/login" replace />;
+//   }
+
+//   return children;
+// }
+
+
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({ children }) {
